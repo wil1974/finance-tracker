6df88427626685bd83ae0e8973752aee5c81c227
focus <williamlim74@gmail.com>
2019-06-23 10:43:28 +0000

    refactor error flash msgs

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,17 @@
 class StocksController < ApplicationController
     def search
-        if params[:stock].present?
-            @stock = Stock.new_from_lookup(params[:stock])
-#            if stock search returned a valid result 
-            if @stock  
-            #       render json: @stock
-#                render "users/my_portfolio"
-#using ajax, so we don't reload my_portfolio page, but reload the partial result page
-                respond_to do |format|
-                    format.js { render partial: 'users/result' }
-                end
-            else
-                flash[:danger] = "You have entered an incorrect symbol"
-                redirect_to my_portfolio_path
-            end
+        if params[:stock].blank?
+                flash.now[:danger] = "You have entered an Empty  Search String"
         else
-            flash[:danger] = "You have entered an Empty  Search String"
-            redirect_to my_portfolio_path
+            @stock = Stock.new_from_lookup(params[:stock])
+#            if stock search returned an invalid result 
+             flash.now[:danger] = "You have entered an incorrect symbol"  unless @stock          
+         end
+         
+#        render partial for ajax
+        respond_to do |format|
+            format.js { render partial: 'users/result' }
         end
     end
+    
 end
