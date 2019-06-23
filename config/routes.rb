ef3ccfe1ca33d405b931835e780eb1b6371865d0
focus <williamlim74@gmail.com>
2019-06-23 13:24:56 +0000

    extend device registration to include full name

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users  #,   :controllers => { :registrations => "registrations" }
 #  get 'welcome/index'
 root 'welcome#index'
 get 'my_portfolio', to: "users#my_portfolio"
 get 'search_stocks', to: "stocks#search"
 
-resources :user_stocks, only: [:create]
+resources :user_stocks, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
